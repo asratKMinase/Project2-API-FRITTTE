7ec0fede59e80fd0ba8f1fe30a2e52cc4a67c91a
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-09 15:57:50 -0500

    Created the create and delete methods in the creditcard servlet as we are not doing update yet and it does not make sense to find all or findbyid customers creditcards. Also added checks in the creditcardservice layer to check to see if any of the ints entered are less than zero.

--- FRITTTE/src/main/java/com/revature/frittte/creditcard/CreditCardServlet.java
+++ FRITTTE/src/main/java/com/revature/frittte/creditcard/CreditCardServlet.java
@@ -1,4 +1,30 @@
 package com.revature.frittte.creditcard;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CreditCardServlet {
+
+    private final CreditCardService creditCardService;
+
+    public CreditCardServlet(CreditCardService creditCardService) {
+        this.creditCardService = creditCardService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<CreditCard> CreateCreditCard(@RequestBody CreditCard newCreditCard){
+        CreditCard creditCard = creditCardService.create(newCreditCard);
+        return new ResponseEntity<>(creditCard, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public void DeleteCreditCard(@RequestBody String deletedCreditCard){
+        creditCardService.delete(deletedCreditCard);
+    }
 }
