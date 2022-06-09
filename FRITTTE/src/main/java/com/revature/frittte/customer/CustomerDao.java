9a9a87205c22f762fddb130fb36764869b79168a
Asrat Minase <asrat461@revature.net>
2022-06-08 19:36:00 -0500

    This is updated Fritte api

---
+++ FRITTTE/src/main/java/com/revature/frittte/customer/CustomerDao.java
@@ -0,0 +1,10 @@
+package com.revature.frittte.customer;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDao extends CrudRepository <Customer, String>{
+
+}
+
