a8bff6912ab4faf92ab0186266da9cc81c4cf31e
Ay Sebirka <ay914@revature.net>
2022-06-09 18:06:38 -0500

    final Skeletons

--- FRITTTE/src/main/java/com/revature/frittte/message/MessageDao.java
+++ FRITTTE/src/main/java/com/revature/frittte/message/MessageDao.java
@@ -1,7 +1,6 @@
 package com.revature.frittte.message;
 
-import com.revature.frittte.customer.Customer;
-import com.revature.frittte.food.Food;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
